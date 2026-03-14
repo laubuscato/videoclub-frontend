826c2007daa875ebff1c19ce5fe4bba1a6d276d0
laubuscato <laubuscato@gmail.com>
2026-03-14 20:25:01 +0100

    actualizacion home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,74 @@
+import "./home.css";
+
 function Home() {
-
     return (
-        <div>
-        <h1>Videoclub</h1>
-        <p>Bienvenido</p>
-        </div>
-    )
+        <div className="home">
 
+            <aside className="sidebar">
+
+                <div className="logo">VIDEOCLUB</div>
+
+                <div className="menu-item">Home</div>
+
+                <div className="menu-item">Géneros</div>
+
+                <ul className="genres">
+                    <li>Fantástico</li>
+                    <li>Terror</li>
+                    <li>Suspense</li>
+                    <li>Drama</li>
+                    <li>Comedia</li>
+                    <li>Acción</li>
+                    <li>Aventuras</li>
+                    <li>Western</li>
+                    <li>Bélico</li>
+                    <li>Histórico</li>
+                    <li>Anime</li>
+                    <li>Musicales</li>
+                    <li>Documentales</li>
+                    <li>Cine infantil</li>
+                </ul>
+
+            </aside>
+
+
+            <header className="header">
+
+                <input
+                    className="search"
+                    placeholder="Search"
+                />
+
+                <div className="header-actions">
+                    <span>Mi cuenta</span>
+                    <span>Mi cesta</span>
+                </div>
+
+            </header>
+
+
+            <main className="content">
+
+                <h2 className="section-title">LO MÁS VENDIDO</h2>
+
+                <div className="movies">
+
+                    <div className="movie"></div>
+                    <div className="movie"></div>
+                    <div className="movie"></div>
+                    <div className="movie"></div>
+
+                    <div className="movie"></div>
+                    <div className="movie"></div>
+                    <div className="movie"></div>
+                    <div className="movie"></div>
+
+                </div>
+
+            </main>
+
+        </div>
+    );
 }
 
-export default Home
+export default Home;
